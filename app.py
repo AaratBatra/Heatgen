dfc4809b0b314cb2e489539ec750cf14c582a9fb
AGRIM KULSHRESHTHA <87011194+FINESTMOSAIC@users.noreply.github.com>
2024-07-14 23:21:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return  "<h1>Working</h1>"
-
+@app.route('/index2')
+def index2():
+    return "heelo "
 
 if __name__ == '__main__':
     app.run(debug=True)
