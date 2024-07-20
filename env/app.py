84d0ce61963ca5963236d3a3c3dfb95109ba4303
adityac2542 <adityac2542@gmail.com>
2024-07-20 17:43:25 +0530

    adding testing module

--- env/app.py
+++ env/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
-
-
-
+import json
+import datetime
 
 app = Flask(__name__)
 
@@ -12,8 +11,15 @@ def index():
 def index2():
     return "heelo "
 
+@app.route('/index3')
+def index3():
+    with open("E:\/fossproject\data.json") as file:
+        data = json.load(file)
+    time_stamp = datetime.datetime.now()
+    
+    return str(time_stamp)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-def func():
-    pass
+
